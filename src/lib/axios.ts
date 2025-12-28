6d0a3c4b0ae531fc96e77f99d309844860a6edbe
happynewyeareveryone <165658644+happynewyeareveryone@users.noreply.github.com>
2025-12-29 04:30:18 +0800

    Fix Axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios"
 import type { AxiosInstance } from 'axios'
 
+const API = "https://suggest-ai.on-forge.com"
+
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API,
     withCredentials: true,
     headers: {
         'Accept': 'application/json',
